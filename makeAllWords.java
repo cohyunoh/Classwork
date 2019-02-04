1fa78507285aef9d07f0ee769d63fef0e400ad7c
cohyunoh <coh00@stuy.edu>
2019-02-04 11:42:37 -0500

    made first version of makeAllWords

--- makeAllWords.java
+++ makeAllWords.java
@@ -1,3 +1,25 @@
+import java.util.List;
+import java.util.ArrayList;
 public class makeAllWords{
-  public static String[] makeAllWords
+  public static List <String> makeAllWords(int k, int maxnum){
+    List<String> ans = new ArrayList<String>();
+    char start = 'a';
+    for(int i = 0; i < maxnum; i++){
+      char newstart = (char)(start + i);
+      makeHelper(k - 1, maxnum, "" + newstart, ans);
+    }
+    return ans;
+  }
+
+  private static void makeHelper(int k, int maxnum, String word, List<String> L){
+    if(k == 0){
+      L.add(word);
+    }else{
+      char start = 'a';
+      for(int i = 0; i < maxnum; i++){
+        char newstart = (char)(start + i);
+        makeHelper(k - 1, maxnum, word + newstart, L);
+      }
+    }
+  }
 }
